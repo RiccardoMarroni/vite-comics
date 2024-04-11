a74bc365d87bdefd80c239ba4543ed64d8ff81f1
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-04-11 13:09:08 +0200

    jumbotron

---
+++ src/components/CardTest.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+qui va il layout della card
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CardTest'
+        props:['gruppi in data']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,45 @@
+<!-- MOLISANA -->
+
+
+<template>
+    <main>
+        <div class="container">
+            <h2 >lista prodotti</h2> 
+            <div class="row">
+                <div class="col-12" v-for="(item, index) in products" :key="index" >
+                    <CardTest />
+                </div>
+            </div>
+            
+        </div>
+    </main>
+</template>
+
+<script>
+import {prodotti} from '../data/store.js'; //import dei dati
+import CardTest from '../component/CardTest.vue';
+    export default {
+        name: 'MainComponent',
+        data() {
+            return{
+                products: prodotti
+            }
+        },
+        mounted(){
+            console.log(this.products);
+        }
+    }
+    
+     
+</script>
+
+
+
+<style lang="scss" scoped >
+main{
+    background-color: black;
+    height: 200px;
+    color:white;
+    margin-top: 50px;
+}
+</style>
